3107af34693a33fb42d4700ab6b47a0757d82c3e
Aeguez <112586458+Aeguez@users.noreply.github.com>
2022-11-13 21:23:06 -0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,201 +1,200 @@
-﻿ class Program {
-  
- static List<bool> TaskIsCompleted = new List<bool>()
-            {
-                true,
-                true,
-                true,
-                true,
-                true,
-                true,
-                true,
-                true,
-                true,
-                true,
-            };
+ partial class Program{
 
- static List<DateTime> StartTime = new List<DateTime>()
- {
-   DateTime.MinValue,
-   DateTime.MinValue,
-   DateTime.MinValue,
-   DateTime.MinValue,
-   DateTime.MinValue,
-   DateTime.MinValue,
-   DateTime.MinValue,
-   DateTime.MinValue,
-   DateTime.MinValue,
-   DateTime.MinValue, 
- };
 
- static List<DateTime> EndTime = new List<DateTime>()
- {
-   DateTime.MinValue,
-   DateTime.MinValue,
-   DateTime.MinValue,
-   DateTime.MinValue,
-   DateTime.MinValue,
-   DateTime.MinValue,
-   DateTime.MinValue,
-   DateTime.MinValue,
-   DateTime.MinValue,
-   DateTime.MinValue, 
- };
- static List<string> Tasks = new List<string>() {
-            "Wake up",
-            "Have breakfast",
-            "Brush your teeth",
-            "Go for classes",
-            "Take a break",
-            "Have lunch",
-            "Do homework",
-            "Go to the gym",
-            "Study",
-            "Go to bed"
-    };
-  
-  //prints all task descriptions one on a line, filtering based on their completion state
-  static void printTasksByCompletion(bool completed){
-    //loop over all the tasks
-    for(int i = 0; i < Tasks.Count; i++){
-      //if this task's completion flag matches our filter, print it
-      if(TaskIsCompleted[i] == completed){
-        String msg = Tasks[i];
-        //if the task has been completed, print its start and end times too
-        if(TaskIsCompleted[i]){
-          msg = msg + $"\tStarted {StartTime[i]}, finished {EndTime[i]}";
-        }
-        Console.WriteLine(msg);
-      }
-    }
-  }
-  
-  
-  
-  
+static void TaskIsCompleted(int taskNum)
+    {
+      string selectedTaskName = Tasks[taskNum];
+      Console.WriteLine($"{selectedTaskName}: ");
+  }
+
+
+
+
+
+
+static List<DateTime> StartTime = new List<DateTime>()
+{
+DateTime.MinValue,
+DateTime.MinValue,
+DateTime.MinValue,
+DateTime.MinValue,
+DateTime.MinValue,
+DateTime.MinValue,
+DateTime.MinValue,
+DateTime.MinValue,
+DateTime.MinValue,
+DateTime.MinValue,
+};
+
+
+
+
+
+static List<DateTime> EndTime = new List<DateTime>()
+{
+DateTime.MinValue,
+DateTime.MinValue,
+DateTime.MinValue,
+DateTime.MinValue,
+DateTime.MinValue,
+DateTime.MinValue,
+DateTime.MinValue,
+DateTime.MinValue,
+DateTime.MinValue,
+DateTime.MinValue,
+};
+static List<string> Tasks = new List<string>() {
+"Wake up",
+"Have breakfast",
+"Brush your teeth",
+"Go for classes",
+"Take a break",
+"Have lunch",
+"Do homework",
+"Go to the gym",
+"Study",
+"Go to bed",
+};
+
+
+
+
+
+static bool Completed(int taskNum) {
+DateTime now = DateTime.Now;
+ if(now >= EndTime[taskNum])
+ {
+ return true;
+ }
+ else {
+    return false;
+}
+}
+
+
+
+
 static void Main(string[] args)
 {
 
+
+
+
+
 bool exit = false;
-
-Console.WriteLine("Welcome to your to-do board. Here are your tasks for today: ");  
-
-
-    bool Completed() {
-          if(TaskIsCompleted[0] == true)
-          Console.WriteLine("Completed");
-          return true;
-      } 
-    foreach(string list in Tasks){
-      Console.WriteLine(list);
-    }
+Console.WriteLine("Welcome to your to-do board. Here are your tasks for today: ");
 
 
-while(!exit) {
 
-  Console.Write("Please select the task you did already");
-  //loop over the data store and print out a menu entry for each task we're tracking
-  for(int i = 0; i < Tasks.Count; i++){
-    //print the array index and description of each task
-    String menuItem = $"{i}: {Tasks[i]}";
-    //if the task is already done, denote that
-    if(TaskIsCompleted[i]){
-      menuItem = menuItem + $" (done at {EndTime[i]})";
-    }
-    Console.WriteLine(menuItem);
-  }  
-       
-        int opCode = int.Parse(Console.ReadLine());
-        Console.WriteLine(); 
 
-        DateTime now = DateTime.Now;
-        string time = now.GetDateTimeFormats('t')[0];
-        DateTime startTime = DateTime.Now;
-        DateTime endTime;
-        startTime = DateTime.Now;
-        endTime = startTime.AddMinutes(10);
-        TimeSpan t = endTime - startTime;
 
-  if (opCode >= 1 && opCode <= 10) 
-        { 
-  switch(opCode){
-    case 1: 
-    Console.WriteLine(Completed());
-    Console.WriteLine($"Your start time {startTime}");
-    Console.WriteLine($"Your end time {endTime}");
-    Console.WriteLine(t);
-    // Activities.Remove(Activities[0]);
-    // Console.WriteLine(result);
-    break;
-    case 2: 
-    Console.WriteLine(Completed());
-    Console.WriteLine($"Your start time {startTime}");
-    Console.WriteLine($"Your end time {endTime}");
-    Console.WriteLine(t);
-    break;
-    case 3:
-    Console.WriteLine(Completed()); 
-    Console.WriteLine($"Your start time {startTime}");
-    Console.WriteLine($"Your end time {endTime}");
-    Console.WriteLine(t);
-    break;
-    case 4: 
-    Console.WriteLine(Completed());
-    Console.WriteLine($"Your start time {startTime}");
-    Console.WriteLine($"Your end time {endTime}");
-    Console.WriteLine(t);
-    break;
-    case 5: 
-    Console.WriteLine(Completed());
-    Console.WriteLine($"Your start time {startTime}");
-    Console.WriteLine($"Your end time {endTime}");
-    Console.WriteLine(t);
-    break;
-    case 6: 
-    Console.WriteLine(Completed());
-    Console.WriteLine($"Your start time {startTime}");
-    Console.WriteLine($"Your end time {endTime}");
-    Console.WriteLine(t);
-    break;
-    case 7: 
-    Console.WriteLine(Completed());
-    Console.WriteLine($"Your start time {startTime}");
-    Console.WriteLine($"Your end time {endTime}");
-    Console.WriteLine(t);
-    break;
-    case 8: 
-    Console.WriteLine(Completed());
-    Console.WriteLine($"Your start time {startTime}");
-    Console.WriteLine($"Your end time {endTime}");
-    Console.WriteLine(t);
-    break;
-    case 9: 
-    Console.WriteLine(Completed());
-    Console.WriteLine($"Your start time {startTime}");
-    Console.WriteLine($"Your end time {endTime}");
-    Console.WriteLine(t);
-    break;
-    case 10: 
-    Console.WriteLine(Completed());
-    Console.WriteLine($"Your start time {startTime}");
-    Console.WriteLine($"Your end time {endTime}");
-    Console.WriteLine(t);
-    break;
-    case 11: 
-    Console.WriteLine("Good bye!"); 
-    exit = true; 
-    Console.ReadKey();
-    Console.Clear();
-    break; 
-    default:
-    Console.WriteLine("Invalid choice. Try again."); 
-    break;
+ foreach(string list in Tasks){
+ Console.WriteLine(list);
+ }
 
-        }
-       
-    }
-  }
-}
-}
-  
+
+
+
+    while(!exit) {
+
+
+
+
+
+        Console.WriteLine("Please select a task");
+        //loop over the data store and print out a menu entry for each task we're tracking
+        for(int i = 0; i < Tasks.Count; i++){
+          //print the array index and description of each task
+          String menuItem = $"{i}: {Tasks[i]}";
+          //also print the completion state of the item
+          if(EndTime[i]>DateTime.MinValue){
+            menuItem = menuItem + $" (Completed at {EndTime[i]})";
+          }
+          else if(StartTime[i]>DateTime.MinValue){
+             menuItem = menuItem + $" (Started at {StartTime[i]})";
+          }
+          else {
+             menuItem = menuItem + " (Not Started)";
+          }
+          
+          Console.WriteLine(menuItem);
+        }
+        Console.WriteLine($"{Tasks.Count}: Exit");
+
+
+
+
+ int opCode = int.Parse(Console.ReadLine());
+ Console.WriteLine();
+
+
+
+
+
+ DateTime now = DateTime.Now;
+ string time = now.GetDateTimeFormats('t')[0];
+ DateTime startTime = DateTime.Now;
+ DateTime endTime;
+ startTime = DateTime.Now;
+ endTime = startTime.AddMinutes(10);
+ TimeSpan t = endTime - startTime;
+
+
+
+
+
+      if(opCode>=0 && opCode < Tasks.Count)
+      {
+          if (Completed(opCode)) {
+            TaskIsCompleted(opCode);
+          }
+      }
+      else if (opCode == Tasks.Count) {
+         Console.WriteLine("Goodbye!");
+         Console.ReadKey();
+         Console.Clear();
+      }
+   
+    //add submenu here, to handle task specific updates
+    Console.WriteLine("How would you like to update the task?");
+    Console.WriteLine("   (1) Start the task");
+    Console.WriteLine("   (2) Finish the task");
+    Console.WriteLine("   (3) Reset the task");
+    Console.WriteLine("   (4) No changes, nevermind");
+ 
+
+    int subCode = int.Parse(Console.ReadLine());
+    
+    switch(subCode){
+      case 1: //start the task
+        StartTime[opCode] = DateTime.Now;
+        break;
+
+      
+      case 2: //end the task
+        EndTime[opCode] = DateTime.Now;
+        break;
+
+
+      case 3: //reset the task
+        StartTime[opCode] = DateTime.MinValue;
+        EndTime[opCode] = DateTime.MinValue;
+        break;
+     
+      case 4:
+        break; //no operation, user changed their mind
+
+
+      default: //bad choice!
+        Console.WriteLine("That was an invalid choice.");
+        break;
+    }
+    
+
+
+    }
+  }
+ }
+
+	
 
